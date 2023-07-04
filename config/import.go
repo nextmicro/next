8a47aecb1ddfcf37c106ddba76f0a6e31d940b85
JellyTony <bytedev@qq.com>
2023-07-04 23:58:16 +0800

    fix

--- config/import.go
+++ config/import.go
@@ -2,6 +2,9 @@ package config
 
 import (
 	_ "github.com/nextmicro/next/middleware/bbr"
+	_ "github.com/nextmicro/next/middleware/circuitbreaker"
+	_ "github.com/nextmicro/next/middleware/logging"
 	_ "github.com/nextmicro/next/middleware/metadata"
 	_ "github.com/nextmicro/next/middleware/recovery"
+	_ "github.com/nextmicro/next/middleware/tracing"
 )
